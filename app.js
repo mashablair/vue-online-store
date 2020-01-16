5c0a6abade4fc0d811d7405073b8dcc516481993
Maria Blair <mashablair@gmail.com>
2020-01-16 14:17:39 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+console.log("hellow form app.js!");
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    product: "Socks",
+    image: "socks.jpg",
+    alt: "socks",
+    inventory: 100,
+    onSale: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg"
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart++;
+    },
+    removeFromCart() {
+      this.cart--;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage; // it swaps a src attr
+    }
+  }
+});
